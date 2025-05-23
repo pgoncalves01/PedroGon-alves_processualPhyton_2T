e06b4391b3078a3ecec3a7847e4ac6a6249d6de3
pgoncalves01 <159036774+pgoncalves01@users.noreply.github.com>
2025-05-23 10:25:35 -0300

    Create desafio1.py

---
+++ desafio1.py
@@ -0,0 +1,20 @@
+print ("Ensira seu nome...")
+nome = input("Qual seu nome? ")
+print(f"Olá {nome}")
+
+print ("Ensira sua idade...")
+idade = input("Qual sua idade? ")
+print (f"Você tem {idade} anos")
+
+print ("Ensira o seu CPF...")
+CPF = input("Qual seu CPF?" )
+print (f"Seu CPF é {CPF}")
+
+print ("Ensira sua altura...")
+altura = input("Qual a sua altura? ")
+print (f"Você tem {altura} de altura")
+
+print ("Ensira seu CEP...")
+CEP = input("Qual o seu CEP? ")
+print (f"Seu CEP é {CEP}")
+
