1706ae10683a303e8923cd175c83008089d51b38
pgoncalves01 <159036774+pgoncalves01@users.noreply.github.com>
2025-05-28 10:05:49 -0300

    Update desafio4.py

--- desafio4.py
+++ desafio4.py
@@ -1,25 +1,77 @@
-def is_prime(n):
-    if n <= 1:
-        return False
-    if n == 2:
-        return True
-    if n % 2 == 0:
-        return False
-    for i in range(3, int(n**0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
-
-numeros_str = [str(i) for i in range(1, 101)]  
-
-numeros_int = []
-for num_str in numeros_str:
-    num = int(num_str)
-    numeros_int.append(num)
-    if is_prime(num):
-        print(f"{num} - Número primo")
-
-print("\nNúmeros divisíveis por 2:")
-for num in numeros_int:
-    if num % 2 == 0:
-        print(num, end=" ")
+for i in range(1,101):
+    print(i)
+    if i == 2:
+        print("primo")
+        
+    if i == 3:
+        print("primo")
+        
+    if i == 5:
+        print("primo")
+        
+    if i == 7:
+        print("primo")
+        
+    if i == 11:
+        print("primo")
+        
+    if i == 13:
+        print("primo")
+        
+    if i == 17:
+        print("primo")
+        
+    if i == 19:
+        print("primo")
+        
+    if i == 23:
+        print("primo")
+        
+    if i == 29:
+        print("primo")
+        
+    if i == 31:
+        print("primo")
+        
+    if i == 37:
+        print("primo")
+        
+    if i == 41:
+        print("primo")
+        
+    if i == 43:
+        print("primo")
+        
+    if i == 47:
+        print("primo")
+        
+    if i == 53:
+        print("primo")
+        
+    if i == 59:
+        print("primo")
+        
+    if i == 61:
+        print("primo")
+        
+    if i == 67:
+        print("primo")
+        
+    if i == 71:
+        print("primo")
+        
+    if i == 73:
+        print("primo")
+        
+    if i == 79:
+        print("primo")
+        
+    if i == 83:
+        print("primo")
+        
+    if i == 89:
+        print("primo")
+        
+    if i == 97:
+        print("primo")
+        
